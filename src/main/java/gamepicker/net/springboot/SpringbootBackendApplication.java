3a25df8194b8e13b7eb2a27e8be4a2eab4cba531
SourcecodeWhisperer <nasiem2@hotmail.de>
2022-02-11 14:23:32 +0100

    Final Update

--- src/main/java/gamepicker/net/springboot/SpringbootBackendApplication.java
+++ src/main/java/gamepicker/net/springboot/SpringbootBackendApplication.java
@@ -21,7 +21,7 @@ public class SpringbootBackendApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Game game1 = Game.builder()
 				.id(12)
-				.genre(Genre.Action)
+				.genre(Genre.Horror)
 				.name("Dark Souls 2")
 				.platform(Plattform.XBOXONE)
 				.rating(4).build();
@@ -31,21 +31,21 @@ public class SpringbootBackendApplication implements CommandLineRunner {
 				.genre(Genre.Action)
 				.name("Dark Souls 3")
 				.platform(Plattform.Boardgames)
-				.rating(4).build();
+				.rating(2).build();
 
 		Game game2 = Game.builder()
 				.id(11)
 				.genre(Genre.Action)
-				.name("Mongo")
+				.name("Mario Cart")
 				.platform(Plattform.PS5)
-				.rating(4).build();
+				.rating(5).build();
 
 		Game game3 = Game.builder()
 				.id(13)
-				.genre(Genre.Action)
-				.name("Dark Souls")
+				.genre(Genre.Strategy)
+				.name("Zelda")
 				.platform(Plattform.PC)
-				.rating(4).build();
+				.rating(3).build();
 
 		Game game4 = Game.builder()
 				.id(15)
@@ -59,7 +59,43 @@ public class SpringbootBackendApplication implements CommandLineRunner {
 				.genre(Genre.Action)
 				.name("Counter Strike")
 				.platform(Plattform.PS4)
-				.rating(4).build();
+				.rating(1).build();
+
+
+		Game game6 = Game.builder()
+				.id(113)
+				.genre(Genre.Action)
+				.name("Monopoly")
+				.platform(Plattform.Boardgames)
+				.rating(1).build();
+
+		Game game7 = Game.builder()
+				.id(114)
+				.genre(Genre.JumpAndRun)
+				.name("Mario")
+				.platform(Plattform.NintendoSwitch)
+				.rating(2).build();
+
+		Game game8 = Game.builder()
+				.id(115)
+				.genre(Genre.Action)
+				.name("Runescape")
+				.platform(Plattform.PC)
+				.rating(5).build();
+
+		Game game9 = Game.builder()
+				.id(116)
+				.genre(Genre.Retro)
+				.name("Half-Life2")
+				.platform(Plattform.PC)
+				.rating(3).build();
+
+		Game game10 = Game.builder()
+				.id(117)
+				.genre(Genre.Retro)
+				.name("Half-Life")
+				.platform(Plattform.PC)
+				.rating(1).build();
 
 
 		gameRepository.save(game1);
@@ -68,5 +104,11 @@ public class SpringbootBackendApplication implements CommandLineRunner {
 		gameRepository.save(game4);
 		gameRepository.save(game5);
 		gameRepository.save(game0);
+		gameRepository.save(game6);
+		gameRepository.save(game7);
+		gameRepository.save(game8);
+		gameRepository.save(game9);
+		gameRepository.save(game10);
+
 	}
 }
